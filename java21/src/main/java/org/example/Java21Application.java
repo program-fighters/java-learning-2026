5585414f837ef6d53abaf98325fddb927d4e3112
sanjeev4.jha <sanjeev4.jha@ril.com>
2026-06-15 12:05:38 +0530

    new module added

---
+++ java21/src/main/java/org/example/Java21Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Java21Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springboot3/src/main/java/org/example/SpringBoot3Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringBoot3Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
